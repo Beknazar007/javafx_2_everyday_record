e2f3b73828a8a9d5a78b8c6c50adbb73d8f86b7b
Bekanzar007 <saitovbeknazar6@gmail.com>
2022-01-08 00:54:54 +0600

    Photo Hero

--- src/main/java/com/example/newrecord/Ala_Too_Diary.java
+++ src/main/java/com/example/newrecord/Ala_Too_Diary.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class RecordingApplication extends Application {
+public class Ala_Too_Diary extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(RecordingApplication.class.getResource("Signin.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Ala_Too_Diary.class.getResource("Signin.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 700, 550);
-        stage.setTitle("Record!");
+        stage.setTitle("Diary");
         stage.setScene(scene);
         stage.show();
     }
